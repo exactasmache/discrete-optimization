6b7f7c1578ae564eccfb56af2ba004b3f5b8f4e5
mbianchetti <bianchetti.ml@gmail.com>
2019-08-28 11:09:41 -0300

    Week 2 succesfully finished

--- Coursera/Discrete_Optimization_Course/Week-02-knapsack/solver.py
+++ Coursera/Discrete_Optimization_Course/Week-02-knapsack/solver.py
@@ -4,6 +4,8 @@
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight', 'benefit'])
 
+debug = True
+
 def calculate_upperbound(items, K):
     value = 0
     optimal = 0
@@ -18,12 +20,16 @@ def calculate_upperbound(items, K):
             break
     return value
 
-def fill_knapsack_groovy(items, capacity):
+def fill_knapsack_greedy(items, capacity, amount_items=None):
+    if amount_items is None:
+      amount_items = len(items)
+    
     # I order the values descendant by benefit
     # I don't need to break equalities. It's the same for me.
     value = 0
     weight = 0
-    taken = [0]*len(items)
+    optimal = 0
+    taken = [0 for j in xrange(amount_items)]
 
     for item in items:
         if weight + item.weight <= capacity:
@@ -33,24 +39,36 @@ def fill_knapsack_groovy(items, capacity):
     
     return value, taken, optimal
 
-def fill_knapsack_dinamically(items, capacity):
+def fill_knapsack_heuristically(items, capacity):
+    amount_items = len(items)
+    
     # I order the values descendant by benefit
     # I don't need to break equalities. It's the same for me.
     items.sort(key=lambda item: item.benefit, reverse=True)
-    min_value_item = min(items, key= lambda item: item.value)
-    min_weight_item = min(items, key= lambda item: item.weight)
-    max_benefit_item = max(items, key= lambda item: item.benefit)
-    max_weight_item = max(items, key= lambda item: item.weight)
-    max_value_item = max(items, key= lambda item: item.value)
     
-    # upper_bound = calculate_upperbound(items, capacity)
-    # print capacity
-    # print upper_bound
-    # print min_value_item
-    # print min_weight_item
-    # print max_value_item
-    # print max_weight_item
-    # print max_benefit_item
+    # I remove elements heavier than the allowed capacity or 
+    # those which their values overpass the upper_bound:
+    upper_bound = calculate_upperbound(items, capacity)
+    items = filter(lambda i: i.weight < capacity and i.value < upper_bound, items)
+    # items = filter(lambda i: i.weight > capacity*.02 and i.weight < capacity*.98, items)
+    
+    value, taken, optimal = fill_knapsack_greedy(items, capacity, amount_items)
+
+    return value, taken, optimal
+
+
+def fill_knapsack_dinamically(items, capacity, amount_items=None):
+    
+    # I order the values descendant by benefit
+    # I don't need to break equalities. It's the same for me.
+    items.sort(key=lambda item: item.benefit, reverse=True)
+    
+    # I remember the amount of items, because I am going to remove some of them.
+    if amount_items is None:
+      amount_items = len(items)
+    
+    # I remove elements heavier than the allowed capacity:
+    items = filter(lambda i: i.weight < capacity, items)
 
     # I need to fill the table recording the values I used in each column
     # For each item I have to decide for each capacity if I put it into the knapsack or not.
@@ -60,7 +78,7 @@ def fill_knapsack_dinamically(items, capacity):
     # Remember that in math notation the first index of a matrix 
     # indicates the row, and the second indicates the column.
 
-    taken = [0 for j in xrange(len(items))]
+    taken = [0 for j in xrange(amount_items)]
     values_table = [[0 for j in xrange(len(items)+1)] for i in xrange(capacity)]
     weights_table = [[0 for j in xrange(len(items)+1)] for i in xrange(capacity)]
 
@@ -115,18 +133,30 @@ def fill_knapsack_dinamically(items, capacity):
     optimal = 1
     i = max_i
     c = max_c
+    if debug:
+      calculated_weight = 0
+      calculated_value = 0
+    
     while i >= 0:
       while c >= 0:
         if values_table[c][i] != values_table[c][i-1]:
-            taken[i-1] = 1
+            if debug:
+              calculated_weight += items[i-1].weight
+              calculated_value += items[i-1].value
+              item_idx = items[i-1].index
+              # print 'estoy en la columna', i, 'que apunta al item', i-1, items[i-1]
+            taken[item_idx] = 1
             c -= items[i-1].weight
         break
       i -= 1
-
-    print sum([items[t].weight for t in taken if t == 1])
-
     
+    if debug:
+      assert calculated_value == max_value
+      assert calculated_weight == max_weight
+      assert max_weight <= capacity
+
     return max_value, taken, optimal
+
 
 def solve_it(input_data):
     # parse the input
@@ -144,9 +174,13 @@ def solve_it(input_data):
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1]), float(parts[0])/int(parts[1])))
 
-    # value, taken, optimal = fill_knapsack_groovy(items, capacity)
-    value, taken, optimal = fill_knapsack_dinamically(items, capacity)
-    
+    # value, taken, optimal = fill_knapsack_greedy(items, capacity)
+    if len(items)<=200:
+        value, taken, optimal = fill_knapsack_dinamically(items, capacity)
+    else :
+        value, taken, optimal = fill_knapsack_heuristically(items, capacity)
+        # value, taken, optimal = fill_knapsack_greedy(items, capacity)
+
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(optimal) + '\n'
     output_data += ' '.join(map(str, taken))
