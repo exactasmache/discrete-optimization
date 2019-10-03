c6132839d8bc4a784b5876d1064bc1aa1a605353
mbianchetti <bianchetti.ml@gmail.com>
2019-10-03 17:40:33 -0300

    Week 3 passed

--- Coursera/Discrete_Optimization_Course/Week-03-coloring/solver.py
+++ Coursera/Discrete_Optimization_Course/Week-03-coloring/solver.py
@@ -9,22 +9,28 @@ class StoreBestObjectiveSolution(cp_model.CpSolverSolutionCallback):
     def __init__(self, variables):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__variables = variables['c']
+        self.__target = variables['obj']
         self.__best_objective = None
         self.__best_solution = None
 
     def on_solution_callback(self):
-        if self.__best_objective == None or self.ObjectiveValue() > self.__best_objective:
+        if self.__best_objective == None or self.ObjectiveValue() < self.__best_objective:
             self.__best_objective = self.ObjectiveValue()
             self.__best_solution = [self.Value(v) for v in self.__variables]
 
     def get_best_solution(self):
-        return {
-          'obj': self.__best_objective, 
-          'values': self.__best_solution
-        }
+        if self.__best_objective == None:
+          return {
+            'obj': len(self.__variables),
+            'values': range(len(self.__variables))
+          }
+        else:
+          return {
+            'obj': self.Value(self.__target)+1,
+            'values': self.__best_solution
+          }
 
-
-def solve(model, variables, max_time=60.0):
+def solve(model, variables, max_time=120.0):
     solver = cp_model.CpSolver()
     solver.parameters.max_time_in_seconds = max_time
     solution = StoreBestObjectiveSolution(variables)
@@ -39,43 +45,57 @@ def solve(model, variables, max_time=60.0):
     else:
         return solution.get_best_solution(), 0
 
-def get_grades(edges, n):
+def calculate_grades(edges, n):
     grade = [0 for v in range(n)]
     for i,j in edges:
         grade[i] += 1
         grade[j] += 1
     return grade
 
+def calculate_neighborhood(edges, n):
+    neighborhood = [[] for v in range(n)]
+    for i,j in edges:
+        neighborhood[i]+=[j]
+        neighborhood[j]+=[i]
+    return neighborhood
+
 def get_unconnected_nodes(G):
-    return [n for n in G['N'] if G['g:N->int'][n] == 0]
+    return [n for n in G['N'] if G['g()'][n] == 0]
 
 def get_leaves(G):
-    return [n for n in G['N'] if G['g:N->int'][n] == 1]
+    return [n for n in G['N'] if G['g()'][n] == 1]
 
-def find_cliques():
-    return
+def could_exist_k_grade_clique(G, k):
+    return len([g for g in G['g()'] if g>=k]) >= k
+
+# TODO: It's not as easy as I thought
+def get_a_k_grade_clique(G, k):
+    if not could_exist_k_grade_clique:
+        return []
+    possibles = [n for n in G['N'] if G['g()'][n] >= k]
+    
+    for i in range(len(possibles)):
+        for n in G['N()'][possibles[i]]:
+            if n not in possibles:
+
+      
 
 
 def make_model(G):
     edges = G['E']
     nodes = G['N']
-    grades = G['g:N->int']
+    grades = G['g()']
 
     n = len(nodes)
 
     # Model definition
     model = cp_model.CpModel()
-    
-    # Domains
-    unconnected = get_unconnected_nodes(G)
-    leaves = get_leaves(G)
 
     # Upper bound
-    UPPER = max(grades)-1
-    u_bs = [0 if i in unconnected else (1 if i in leaves else UPPER) for i in nodes]
+    # UPPER = max(grades)-1
+    u_bs = [grades[i] for i in nodes]
     obj_bound = max(u_bs)
 
-    print(u_bs)
     # Variables: 
     # c_i = j iff the color j is assigned to the node i
     c = [ model.NewIntVar(0, u_bs[i], 
@@ -95,6 +115,11 @@ def make_model(G):
 
     # Objective function: minimize max(c)
     model.Minimize(target+1)
+    # model.Minimize(sum(c))
+
+    model.AddDecisionStrategy(c+[target], 
+                              cp_model.CHOOSE_FIRST,
+                              cp_model.SELECT_MIN_VALUE)
 
     return model, {'c': c, 'obj': target}
 
@@ -125,7 +150,8 @@ def solve_it(input_data):
     G = {
       'N': range(node_count),
       'E': edges,
-      'g:N->int': get_grades(edges, node_count)
+      'g()': calculate_grades(edges, node_count)
+      'N()': calculate_neighborhood(edges, node_count)
     }
     model, variables = make_model(G)
     
