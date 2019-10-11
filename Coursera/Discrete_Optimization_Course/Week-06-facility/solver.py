52a43e5bc0d5ad68cf22fa4da588df7c9b2e22ff
mbianchetti <bianchetti.ml@gmail.com>
2019-10-11 13:25:59 -0300

    Solving with the full version of the cplex

--- Coursera/Discrete_Optimization_Course/Week-06-facility/solver.py
+++ Coursera/Discrete_Optimization_Course/Week-06-facility/solver.py
@@ -4,7 +4,7 @@
 import math
 import cplex
 import cplex.callbacks as cpx_cb
-from docplex.mp.model import Model
+# from docplex.mp.model import Model
 
 from collections import namedtuple
 
@@ -12,50 +12,82 @@ Point = namedtuple("Point", ['x', 'y'])
 Facility = namedtuple("Facility", ['index', 'setup_cost', 'capacity', 'location'])
 Customer = namedtuple("Customer", ['index', 'demand', 'location'])
 
-def get_solution(cpx):
-    print('solving')
-    # print(cpx.problem_type[cpx.get_problem_type()])
-    cpx.write('facility_model.lp', filetype='lp')
-    # cpx.set_results_stream('output', fn=None)
 
-    cpx.parameters.tune.timelimit.set(2)
+def get_solution(facilities, customers, cpx, d_m):
+    obj = None
+    solution = None
+    optimal = 0
+    
+    # CPLEX Parameters
+    cpx.write('facility_model.lp', filetype='lp')   # Write LP model to a file
+    # cpx.set_results_stream('output', fn=None)     # Hide output
+    # cpx.parameters.tune.timelimit.set(1)            # Timelimit in seconds
+    cpx.parameters.timelimit.set(10)
 
     cpx.solve()
-    
-    optimal = False
-    obj = None
-    values = None
-    
     status = cpx.solution.get_status_string()
-    if status == 'integer optimal solution':
-      optimal = True
-      obj = cpx.solution.get_objective_value()
-      y_values = cpx.solution.get_values()
-    else:
-      optimal = False
-      obj = cpx.solution.MIP.get_best_objective()
-      values = [] #TODO
+
+    if 'no integer solution' in status:
+        return get_trivial_solution(facilities, customers)
     
-    return obj, values
+    # I asume that when the solution falls bellow the tolerance parameter it's optimal.
+    if status == 'integer optimal solution' or 'integer optimal, tolerance':
+        optimal = 1
+      
+    obj = cpx.solution.get_objective_value()
+    
+    # I think this returns the best bound, but without the integer solution
+    # obj1 = cpx.solution.MIP.get_best_objective()
+  
+    # calc_objective = sum(
+    #     [w.setup_cost * cpx.solution.get_values('x{}'.format(w.index)) for w in facilities]
+    #     + 
+    #     [d_m[c.index][w.index] * cpx.solution.get_values('y{}_{}'.format(w.index, c.index))
+    #     for c in customers for w in facilities]
+    # )
+    # print(calc_objective)
+
+    solution = [-1] * len(customers)
+    for c in customers:
+        for w in facilities:
+            if int(cpx.solution.get_values('y{}_{}'.format(w.index, c.index))) == 1:
+                solution[c.index] = w.index
+
+    return obj, solution, optimal
+
 
 def length(point1, point2):
     ''' Returns the euclidean distance between two points'''
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
 
-def build_model(facilities, customers):
-    xw = ['x{}'.format(w.index) for w in facilities]
-    ywc = ['y{}{}'.format(w.index, c.index) for w in facilities for c in customers]
 
+def build_distances_matrix(facilities, customers):
+    d_m = [
+        [ length(c.location, w.location) for w in facilities ]
+        for c in customers
+    ]
+    # it returns a matrix to be indexed as:
+    # d_m[c][w]
+    return d_m
+
+
+def build_model(facilities, customers, d_m):
     cpx = cplex.Cplex()
     cpx.set_problem_name('Facility')
 
-    cpx.variables.add(
-        names = xw+ywc, 
-        types=[cpx.variables.type.binary] * (len(xw)+len(ywc))
-    )
+    # VARIABLES and OBJECTIVE FUNCTION
+    xw = ['x{}'.format(w.index) for w in facilities]
+    ywc = ['y{}_{}'.format(w.index, c.index) for c in customers for w in facilities]
+    # ywc ˜= [[y0_0, ..., yn_0], [y0_1, ..., yn_1], ...]
 
+    cpx.variables.add(obj = [w.setup_cost for w in facilities] 
+                          + [d_m[c.index][w.index] for c in customers for w in facilities],
+                      names = xw + ywc,
+                      types=[cpx.variables.type.binary] * (len(xw)+len(ywc)))
+
+    # CONSTRAINTS
     for c in customers:
-        ind = ['y{}{}'.format(w.index, c.index) for w in facilities]
+        ind = ['y{}_{}'.format(w.index, c.index) for w in facilities]
         names = ['C_{}'.format(c.index)]
         val = [1.0] * len(facilities)
         rhs = [1.0]
@@ -67,12 +99,12 @@ def build_model(facilities, customers):
             names = names,
         )
 
-        names = ['W_{}{}'.format(c.index, w.index) for w in facilities]
+        names = ['W_{}_{}'.format(c.index, w.index) for w in facilities]
         senses = ['L'] * len(facilities)
         rhs = [0.0] * len(facilities)
         lin_exp = [ 
             cplex.SparsePair(
-                ind=[ 'y{0}{1}'.format(w.index, c.index), 
+                ind=[ 'y{0}_{1}'.format(w.index, c.index), 
                       'x{0}'.format(w.index)], 
                 val=[1, -1]
             ) for w in facilities 
@@ -84,10 +116,23 @@ def build_model(facilities, customers):
             rhs = rhs,
             names = names,
         )
-    
+    for w in facilities:
+        ind = ['y{}_{}'.format(w.index, c.index) for c in customers]
+        names = ['L_{}'.format(w.index)]
+        val = [c.demand for c in customers]
+        rhs = [w.capacity]
+        senses = ['L']
+
+        cpx.linear_constraints.add(
+          lin_expr = [cplex.SparsePair(ind=ind, val=val)],
+          senses = senses,
+          rhs = rhs,
+          names = names
+        )
+
     return cpx
 
-def solve_it(input_data):
+def parse_input(input_data):
     # parse the input
     lines = input_data.split('\n')
 
@@ -104,12 +149,13 @@ def solve_it(input_data):
     for i in range(facility_count+1, facility_count+1+customer_count):
         parts = lines[i].split()
         customers.append(Customer(i-1-facility_count, int(parts[0]), Point(float(parts[1]), float(parts[2]))))
+    
+    return facilities, customers
 
-    facility_m = build_model(facilities, customers)
-    get_solution(facility_m)
-
+def get_trivial_solution(facilities, customers):
     # build a trivial solution
     # pack the facilities one by one until all the customers are served
+    optimal = 0
     solution = [-1]*len(customers)
     capacity_remaining = [f.capacity for f in facilities]
 
@@ -133,11 +179,26 @@ def solve_it(input_data):
     for customer in customers:
         obj += length(customer.location, facilities[solution[customer.index]].location)
 
+    return obj, solution, optimal
+
+
+def format_solution(obj, solution, optimal):
     # prepare the solution in the specified output format
-    output_data = '%.2f' % obj + ' ' + str(0) + '\n'
+    output_data = '%.2f' % obj + ' ' + str(optimal) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
+
+
+def solve_it(input_data):
+    facilities, customers = parse_input(input_data)
+    
+    d_m = build_distances_matrix(facilities, customers)
+    facility_m = build_model(facilities, customers, d_m)
+
+    obj, solution, optimal = get_solution(facilities, customers, facility_m, d_m)
+
+    return format_solution(obj, solution, optimal)
 
 
 import sys
